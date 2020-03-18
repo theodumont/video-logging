c3dee6f6aeb270711a278da96108b6bd8c02ca9a
Théo Dumont <theo.dumont@mines-paristech.fr>
2020-03-19 00:42:11 +0100

    Added a CLI

---
+++ cli.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import argparse
+try:
+    import numpy as np
+except ImportError:
+    print("---X This tools requires the numpy module, which seems currently unavailable. Please install numpy before tying to use this tool. The recommended syntax to install numpy is :\n$ python3 -m pip install numpy")
+    sys.exit(1)
+try:
+    import derushing as dr  # Our module to command the robot.
+except ImportError:
+    print("---X This command line tool is an interface to a python module called derushing. In order to use this tool you must first make sure that this module is available (it currently isn't). The recommended way to do this is to grab the source code derushing.py and put it in the same directory as this cli.py file. The source code for derushing is available freely on GitHub at https://github.com/theodumont/derushing-python.")
+    sys.exit(1)
+
+
+class CLI(object):
+    """docstring for CLI."""
+
+    def __init__(self, cli_args):
+        # List of all parameters accepted to trigger auto mode.
+        self.clean_list = ["clean", "c", "all", "a"]
+        # Same for manual mode.
+        self.folder_list = ["folder", "f"]
+        # Same for manual mode.
+        self.trash_list = ["trash", "t"]
+        # Same for help.
+        self.date_list = ["sort", "time", "s", "date", "d"]
+        # Same for help.
+        self.help_list = ["help", "HELP", "Help", "h", "H", "?"]
+        # To leave the tool.
+        self.exit_list = ["exit", "e", "leave", "l", "quit", "q"]
+        # folder
+        self.folder_to_sort = cli_args.folder
+
+    def read_command(self, command):
+        # cursor is used to keep track of how many argument we read from the users command.
+        cursor = 0
+        split_command = str.split(command)
+        if len(split_command) == 0:
+            # Empty line, we can just ignore it
+            return
+        # else ...
+        instruction = split_command[0]
+        cursor += 1
+        if instruction in self.clean_list:
+            return self.process_clean()
+        elif instruction in self.folder_list:
+            return self.process_folder()
+        elif instruction in self.trash_list:
+            return self.process_trash(split_command, cursor)
+        elif instruction in self.date_list:
+            return self.process_date()
+        elif instruction in self.help_list:
+            return self.process_help(split_command, cursor)
+        elif instruction in self.exit_list:
+            return self.exit()
+        else:
+            dr.bprint(f"The inputed command {command} could not be parsed, because the tool did not understand the term '{instruction}'. If you wish to you can use :\n'>> help'\nThat instruction will bring a list of the available instruction and their use cases.", 2)
+            return
+
+    def exit(self):
+        # Used to leave the tool.
+        dr.bprint("Leaving the tool...")
+        sys.exit(0)
+
+    def process_folder(self):
+        dr.folder_sort()
+
+    def process_trash(self, split_command, cursor):
+        if len(split_command) == cursor:
+            # i.e. we have no more arguments available
+            dr.bprint(f"What time limit do you want to impose?", 1)
+            dr.bprint(f"The syntax to choose the time limit is:\n'>> trash <time limit>'\nTime limit has to be a positive int value.")
+        else:
+           time_limit = split_command[cursor]
+           cursor += 1
+           try:
+               int_time_limit = int(time_limit)
+               if int_time_limit <= 0:
+                   dr.bprint(f"You asked the tool to take {time_limit} as a time limit, but negative (zero included) values are not valid in that context. Please input a positive integer.", 2)
+                   return
+               else:
+                   dr.trash_videos(int_time_limit)
+           except ValueError as e:
+               dr.bprint(f"The value of the step frequency has to be a positive int, but the tool could not parse {time_limit} as an int. The correct syntax to set the time limit is :\n'>> trash <time limit>'", 2)
+               return
+
+    def process_date(self):
+        dr.sort_videos()
+
+    def process_clean(self, split_command, cursor):
+        self.process_folder()
+        self.process_trash(split_command, cursor)
+        self.process_date()
+
+    def process_help(self, split_command, cursor):
+        return
+
+if __name__ == '__main__':
+    os.system('cls')
+    # Command line arguments parsing
+    parser = argparse.ArgumentParser(description='Command line frontend used to derush before video editing.', epilog="This tool was designed by Théo Dumont and all the source code is available at https://github.com/theodumont/derushing-python under the GPL 3 License.")
+    parser.add_argument("-f", "--folder", type=str, help="folder to sort")
+    args = parser.parse_args()
+
+    header = (
+    "\n"
+    "##############################################################################\n"
+    "  This tool was designed by Théo Dumont and all the source code is available  \n"
+    "  at https://github.com/theodumont/derushing-python under the GPL 3 License.  \n"
+    "##############################################################################\n"
+             )
+    print(header)
+
+    cli = CLI(args)
+    while True:
+        try:
+            dr.bprint("", 3)
+            command = input(">> ")
+            cli.read_command(command)
+        except EOFError:
+            print("exit")  # In order to avoid ugly output
+            cli.exit()
+            break
--- derushing.py
+++ derushing.py
@@ -7,6 +7,7 @@ import os
 import time
 import argparse
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from textwrap import fill
 
 
 DIRS = ['Audio', 'Videos', 'Images', 'Documents', 'Folders', 'Other']
@@ -16,9 +17,6 @@ EXT_IMAGE = ['.jpeg', '.jpg', '.png', '.svg', '.bmp', '.gif']
 EXT_DOCUMENT = ['.txt', '.pdf', '.doc', '.docx', '.odt', '.html', '.md', '.rtf', '.xlsx', '.pptx']
 EXT_FOLDER = ['.rar', '.zip']
 
-
-# limit time for deleting (seconds)
-time_limit = 2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("folder_to_sort", type=str, help="folder to clean")
@@ -33,118 +31,113 @@ args = parser.parse_args()
 os.chdir(args.folder_to_sort)
 
 
-class MyCleaner():
-
+def folder_sort():
     """
-    Cleans a folder.
+    Sorts the files into directories according to their extension.
+    Creates the directories if they don't exist.
     """
+    bprint("Sorting files...\n")
+    moved = "   -> moved"
+    if not os.path.isdir('Audio'):
+        for d in DIRS:
+            os.mkdir('./{}'.format(d))
 
-    def __init__(self, folder_to_sort, time_limit):
-        """
-        Class initialization.
-        """
-        self.folder_to_sort = folder_to_sort
-        self.time_limit = time_limit
+    bprint("  File type    File name\n", 3)
+    bprint(" -------------------------", 3)
+    for file in os.listdir():
+        name, extension = os.path.splitext(file)
 
-    def folder_sort(self):
-        """
-        Sorts the files into directories according to their extension.
-        Creates the directories if they don't exist.
-        """
-        print(">> Sorting files...\n")
-        moved = "   -> moved"
-        if not os.path.isdir('Audio'):
-            for d in DIRS:
-                os.mkdir('./{}'.format(d))
-
-        print("  File type    File name\n"
-              " -------------------------")
-        for file in os.listdir():
-            name, extension = os.path.splitext(file)
-
-            if extension in EXT_AUDIO:
-                print(" Audio          {0}{1}".format(file, moved))
-                os.rename(file, 'Audio/' + file)
-            elif extension in EXT_VIDEO:
-                print(" Video          {0}{1}".format(file, moved))
-                os.rename(file, 'Videos/' + file)
-            elif extension in EXT_IMAGE:
-                print(" Image          {0}{1}".format(file, moved))
-                os.rename(file, 'Images/' + file)
-            elif extension in EXT_DOCUMENT:
-                print(" Document       {0}{1}".format(file, moved))
-                os.rename(file, 'Documents/' + file)
-            else:
-                if os.path.isdir(name) or extension in EXT_FOLDER:
-                    if name not in DIRS:
-                        print(" Folder         {0}{1}".format(file, moved))
-                        os.rename(file, 'Folders/' + file)
-                    else:
-                        print(" Script         {}".format(file))
+        if extension in EXT_AUDIO:
+            bprint(" Audio          {0}{1}".format(file, moved), 3)
+            os.rename(file, 'Audio/' + file)
+        elif extension in EXT_VIDEO:
+            bprint(" Video          {0}{1}".format(file, moved), 3)
+            os.rename(file, 'Videos/' + file)
+        elif extension in EXT_IMAGE:
+            bprint(" Image          {0}{1}".format(file, moved), 3)
+            os.rename(file, 'Images/' + file)
+        elif extension in EXT_DOCUMENT:
+            bprint(" Document       {0}{1}".format(file, moved), 3)
+            os.rename(file, 'Documents/' + file)
+        else:
+            if os.path.isdir(name) or extension in EXT_FOLDER:
+                if name not in DIRS:
+                    bprint(" Folder         {0}{1}".format(file, moved), 3)
+                    os.rename(file, 'Folders/' + file)
                 else:
-                    print(" Other          {0}{1}".format(file, moved))
-                    os.rename(file, 'Other/' + file)
+                    bprint(" Script         {}".format(file), 3)
+            else:
+                bprint(" Other          {0}{1}".format(file, moved), 3)
+                os.rename(file, 'Other/' + file)
+        time.sleep(.04)
+    bprint("Files sorted by type!")
+
+
+def trash_videos(time_limit):
+    """
+    Trashes the videos that are shorter than .time_limit to get rid of
+    all the shooting errors.
+    """
+    if not os.path.isdir('Audio'):
+        folder_sort()
+    bprint("Trashing files of duration <= {}s...\n".format(time_limit))
+    if not os.path.isdir('Videos/Trash'):
+        os.mkdir('Videos/Trash')
+    bprint("  File name      Duration\n", 3)
+    bprint(" -------------------------", 3)
+    for file in os.listdir('./Videos'):
+        if not os.path.isdir('Videos/' + file):
+            clip = VideoFileClip('Videos/' + file)
+            duration = clip.duration
+            clip.close()
             time.sleep(.1)
-        print(">> Files sorted!\n")
-
-    def trash_videos(self):
-        """
-        Trashes the videos that are shorter than self.time_limit to get rid of
-        all the shooting errors.
-        """
-        if not os.path.isdir('Audio'):
-            self.folder_sort()
-        print(">> Trashing files...\n")
-        if not os.path.isdir('Videos/Trash'):
-            os.mkdir('Videos/Trash')
-        print("  File name      Duration\n"
-              " -------------------------")
-        for file in os.listdir('./Videos'):
-            if not os.path.isdir('Videos/' + file):
-                clip = VideoFileClip('Videos/' + file)
-                duration = clip.duration
-                clip.close()
-                time.sleep(.1)
-                deleted = ""
-                if duration < self.time_limit:
-                    os.rename('Videos/' + file, 'Videos/Trash/' + file)
-                    deleted = "   -> moved to '/Trash'"
-                print(" {0}     {1:.1f} s{2}".format(file, duration, deleted))
-        print(">> Files trashed!\n")
-
-    def sort_videos(self):
-        """
-        Sorts videos in directories by date to simplifiy the derushing process.
-        """
-        print(">> Sorting videos by date...\n")
-        if not os.path.isdir('Audio'):
-            self.folder_sort()
-        sep = ' ' + 50*'-'
-        print("File name".center(25) + "      Created on\n"+sep)
-        for file in os.listdir('./Videos'):
-            if not os.path.isdir('Videos/' + file):
-                path = 'Videos/' + file
-                creation = time.localtime(os.path.getmtime(path))
-                destination = time.strftime('%y%m%d-%a', creation)
-                print((file.center(25) + '{0}   -> moved to {1}').format(
-                      time.strftime('%c', creation),
-                      destination))
-                if not os.path.isdir('Videos/' + destination):
-                    os.mkdir('Videos/' + destination)
-                os.rename('Videos/' + file, 'Videos/' + destination + '/' + file)
-
-                time.sleep(.1)
+            deleted = ""
+            if duration < time_limit:
+                os.rename('Videos/' + file, 'Videos/Trash/' + file)
+                deleted = "   -> moved to '/Trash'"
+            bprint(" {0}     {1:.1f} s{2}".format(file, duration, deleted), 3)
+    bprint("Files trashed!")
 
 
-if __name__ == '__main__':
-    # main program
-    print('** Cleanup of the {} folder **\n'.format(args.folder_to_sort))
+def sort_videos():
+    """
+    Sorts videos in directories by date to simplifiy the derushing process.
+    """
+    bprint("Sorting videos by date...\n")
+    if not os.path.isdir('Audio'):
+        folder_sort()
+    sep = ' ' + 50*'-'
+    bprint("File name".center(25) + "      Created on\n"+sep, 3)
+    for file in os.listdir('./Videos'):
+        if not os.path.isdir('Videos/' + file):
+            path = 'Videos/' + file
+            creation = time.localtime(os.path.getmtime(path))
+            destination = time.strftime('%y%m%d-%a', creation)
+            bprint((file.center(25) + '{0}   -> moved to {1}').format(
+                  time.strftime('%c', creation),
+                  destination), 3)
+            if not os.path.isdir('Videos/' + destination):
+                os.mkdir('Videos/' + destination)
+            os.rename('Videos/' + file, 'Videos/' + destination + '/' + file)
 
-    cleaner = MyCleaner(args.folder_to_sort, time_limit)
+            time.sleep(.04)
+    bprint("Videos sorted by date!")
 
-    if args.folder:
-        cleaner.folder_sort()
-    if args.trash:
-        cleaner.trash_videos()
-    if args.date:
-        cleaner.sort_videos()
+def bprint(msg, mode=0):
+    # mode 0 means information, 1 means warning and 2 means error
+    wrapped_msg_list = []
+    for line in iter(msg.splitlines()):
+        wrapped_msg_list.append(fill(line, width=80))
+    wrapped_msg = "\n".join(wrapped_msg_list)
+    if mode == 1:
+        print(f"---! {wrapped_msg}")
+        return
+    elif mode == 2:
+        print(f"---X {wrapped_msg}")
+        return
+    elif mode == 3:
+        print(f"     {wrapped_msg}")
+        return
+    else:   # i.e. mode == 0
+        print(f"---> {wrapped_msg}")
+        return
